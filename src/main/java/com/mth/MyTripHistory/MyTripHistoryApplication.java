680771d49d0284f665906f40219c99259eefaec6
현웅 <hyunwoongkim1234@gmail.com>
2025-02-06 17:33:10 +0900

    업로드

---
+++ src/main/java/com/mth/MyTripHistory/MyTripHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.mth.MyTripHistory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTripHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTripHistoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mth/MyTripHistory/MyTripHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mth.MyTripHistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTripHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

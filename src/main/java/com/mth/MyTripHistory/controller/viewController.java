832f0c5bac2d0ff93070cfc695f4490498c4f8f5
현웅 <hyunwoongkim1234@gmail.com>
2025-02-26 15:12:58 +0900

    세팅 업로드

--- src/main/java/com/mth/MyTripHistory/controller/viewController.java
+++ src/main/java/com/mth/MyTripHistory/controller/viewController.java
@@ -1,13 +1,14 @@
 package com.mth.MyTripHistory.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class viewController {
 
-    @RequestMapping(value="/index")
+    @GetMapping("/index")
     public String getIndexPage(){
         return "index";
     }
